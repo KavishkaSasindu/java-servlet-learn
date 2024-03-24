32c9016bfc299326684e0c274da662f3157a55c4
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-25 01:27:34 +0530

    initialize commit

---
+++ src/main/java/com/servlet/AddServlet.java
@@ -0,0 +1,26 @@
+package com.servlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AddServlet extends HttpServlet{
+	
+	public void service(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		int number1 = Integer.parseInt(request.getParameter("number-1"));
+		int number2 = Integer.parseInt(request.getParameter("number-2"));
+		
+		int sum = number1+number2;
+		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("Sum is "+sum);
+		System.out.println(sum);
+		
+	}
+
+}
